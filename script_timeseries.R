bf0d117c87c74253920c2b98c9f691e4da4142d9
phobeika <p.hobeika@gmail.com>
2026-04-17 15:46:44 +0200

    import des donnees

--- script_timeseries.R
+++ script_timeseries.R
@@ -1,8 +1,25 @@
 # Projet Time-Series
 # 2A Paul Hobeika et Sara Chikhi
 
+# pacakges
+library(dplyr)
+library(readr)
+library(lubridate)
+# import des données
+ind <- read_csv2("donnees/valeurs_mensuelles.csv", col_names = TRUE) %>% 
+  slice(4:437) %>% # on supprime les premières lignes qui ne correspondent pas à des données
+  select(-Codes) %>% # on supprime aussi la colonne "Codes" qui est toujours égale à 'A'
+  rename(Période = 'Libellé', Indice = "Indice CVS-CJO de la production industrielle (base 100 en 2021) - Fabrication de pesticides et d'autres produits agrochimiques (NAF rév. 2, niveau groupe, poste 20.2)") %>% 
+  mutate(Période = ym(Période))
+
+# Graphe de la série
+
+
+
 # Partie I
 #=========
 
 # 1. Que représente la série choisie ?
+
+
 
